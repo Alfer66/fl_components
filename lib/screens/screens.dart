d34c8ea4a14544d7ed1db871387b491e26950d6c
LOCAL\alferr <alferr@lobos.com.mx>
2023-02-15 19:11:38 -0600

    2023_02_15_1911_ARV
    Avence del curso de  flutter

---
+++ lib/screens/screens.dart
@@ -0,0 +1,7 @@
+import 'package:proyec2/screens/home_screen.dart';
+
+export 'package:proyec2/screens/listview1_screen.dart';
+export 'package:proyec2/screens/listview2_screen.dart';
+export 'package:proyec2/screens/alert_screen.dart';
+export 'package:proyec2/screens/card_screen.dart';
+export 'package:proyec2/screens/home_screen.dart';
