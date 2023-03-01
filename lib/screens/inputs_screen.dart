6e14886f60c73940f181ff9e7a2c4aeed89c1849
LOCAL\alferr <alferr@lobos.com.mx>
2023-02-28 19:06:06 -0600

    2023_02_28_1905_ARV
    Avance del curso de Flutter

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_input_field.dart';
+
 class InputsScreen extends StatelessWidget {
   const InputsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+    final Map<String, String> formValues = {
+      'firt_name': 'Alfer',
+      'last_name': 'Romero',
+      'email': 'alferr6@gmail.com',
+      'password': '123456',
+      'role': 'Admin'
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
@@ -12,21 +23,76 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(children: [
-            TextFormField(
-              autofocus: true,
-              initialValue: '',
-              textCapitalization: TextCapitalization.words,
-              onChanged: (value) {
-                print('value: $value');
-              },
-              validator: (value) {
-                if (value == null) return 'Este campo es requerido';
-                return 'Hola Mundo';
-              },
-              autovalidateMode: AutovalidateMode.always,
-            )
-          ]),
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                CustomInputField(
+                  labelText: 'Nombre',
+                  helperText: 'Nombre del usaurio',
+                  formProperty: 'first_name',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Apellido',
+                  helperText: 'Apellido del usuario',
+                  formProperty: 'last_name',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Correo',
+                  helperText: 'Correo del usuario',
+                  keyboardType: TextInputType.emailAddress,
+                  formProperty: 'email',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                CustomInputField(
+                  labelText: 'Contraseña',
+                  helperText: 'Contraseña del usuario',
+                  obscureText: true,
+                  formProperty: 'password',
+                  formValues: formValues,
+                ),
+                const SizedBox(height: 30),
+                DropdownButtonFormField<String>(
+                  value: 'Admin',
+                  items: const [
+                    DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                    DropdownMenuItem(
+                        value: 'Superuser', child: Text('Superuser')),
+                    DropdownMenuItem(
+                        value: 'Developer', child: Text('Developer')),
+                    DropdownMenuItem(
+                        value: 'Jr. Developer', child: Text('Jr. Developer'))
+                  ],
+                  onChanged: (value) {
+                    print(value);
+                    formValues['role'] = value ?? 'Admin';
+                  },
+                ),
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(child: Text('Guardar')),
+                  ),
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(FocusNode());
+
+                    if (!myFormKey.currentState!.validate()) {
+                      print('Formulario no válido');
+                      return;
+                    }
+
+                    //* imprimir valores del formulario
+                    print(formValues);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
---
+++ lib/screens/slider_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SliderScreen extends StatefulWidget {
+  const SliderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SliderScreen> createState() => _SliderScreenState();
+}
+
+class _SliderScreenState extends State<SliderScreen> {
+  double _sliderValue = 100;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Slider & Checks'),
+      ),
+      body: Column(
+        children: [
+          Slider(
+            min: 50,
+            max: 400,
+            value: _sliderValue,
+            onChanged: (value) {
+              setState(() {});
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
