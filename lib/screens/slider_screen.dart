bc59fb47e668b072986ff7ed5959d8177c866868
LOCAL\alferr <alferr@lobos.com.mx>
2023-03-06 19:07:02 -0600

    2023_03_06_1906_ARV
    Avance del curso de Flutter

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class SliderScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnabled = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,14 +19,58 @@ class _SliderScreenState extends State<SliderScreen> {
       ),
       body: Column(
         children: [
-          Slider(
+          Slider.adaptive(
             min: 50,
             max: 400,
+            activeColor: AppTheme.primary,
             value: _sliderValue,
-            onChanged: (value) {
-              setState(() {});
-            },
-          )
+            onChanged: _sliderEnabled
+                ? (value) {
+                    _sliderValue = value;
+                    setState(() {});
+                  }
+                : null,
+          ),
+          // Checkbox(
+          //   value: _sliderEnabled,
+          //   onChanged: (value) {
+          //     _sliderEnabled = value ?? true;
+          //     setState(() {});
+          //   },
+          // ),
+          // Switch(
+          //   value: _sliderEnabled,
+          //   onChanged: (value) => setState(() {
+          //     _sliderEnabled = value;
+          //   }),
+          // ),
+          CheckboxListTile(
+            activeColor: AppTheme.primary,
+            title: const Text('Habilitar Slider'),
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+              _sliderEnabled = value ?? true;
+            }),
+          ),
+          SwitchListTile.adaptive(
+            activeColor: AppTheme.primary,
+            title: const Text('Habilitar Slider'),
+            value: _sliderEnabled,
+            onChanged: (value) => setState(() {
+              _sliderEnabled = value;
+            }),
+          ),
+          const AboutListTile(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: const NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/8/8a/Golden_Retriever_9-year_old.jpg'),
+                fit: BoxFit.contain,
+                width: _sliderValue,
+              ),
+            ),
+          ),
         ],
       ),
     );
